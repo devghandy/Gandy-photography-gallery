a2a7aea577ea3bd7ca1bc1a0c1e48ce574cc46d0
Adigboluja Ganiyat <devghandy@gmail.com>
2022-04-20 11:54:44 +0100

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {useState} from 'react';
+import gandy from './assets/Gandy.jpg'
+import slippers from './assets/G1.jpg'
+import blueSlippers from './assets/G4.jpg'
+import pimpsole from './assets/G5.jpg'
+import belt from './assets/G6.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const  images =[gandy, slippers,  blueSlippers, pimpsole, belt];
+
+
+const Loading = ({ calculatedWidth }) => (
+  <aside>
+    <div className="loading-bar">
+      <label htmlFor="images-loaded">Loading all your favorite images...</label>
+      <progress id="images-loaded" max="100" value={calculatedWidth}></progress>
     </div>
+  </aside>
+);
+
+const App = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [numLoaded, setNumLoaded] = useState(0);
+
+  const handleClick = () => {
+    const length = images.length - 1;
+
+    setCurrentImage((currentImage) =>
+      currentImage < length ? currentImage + 1 : 0
+    );
+  };
+
+  const handleImageLoad = () => {
+    setNumLoaded((numLoaded) => numLoaded + 1);
+  };
+
+  return (
+    <section>
+      <header>
+        <h1>Gandy</h1>
+        <h2>
+          Leather Product <br /> by Ganiyat 
+        </h2>
+      </header>
+
+      <figure>
+        {numLoaded < images.length && (
+          <Loading calculatedWidth={(numLoaded / images.length) * 100} />
+        )}
+        <figcaption>
+          {currentImage + 1} / {images.length}
+        </figcaption>
+        {images.map((imageURL, index) => (
+          <img
+            alt=""
+            key={imageURL}
+            src={imageURL}
+            onClick={handleClick}
+            onLoad={handleImageLoad}
+            className={currentImage === index ? "display" : "hide"}
+          />
+        ))}
+      </figure>
+    </section>
   );
-}
+};
 
 export default App;
